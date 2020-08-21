39bc11baa0fed5435e5dfb11c37e9be63f8b5668
muuminmuumin <izgomakomatw3623@gmail.com>
2020-08-20 22:36:46 -0700

    gitgnore application 修正

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.generators do |g|
       g.stylesheets false
@@ -16,5 +15,6 @@ module ChatSpace
       g.helper false
       g.test_framework false
     end
-
+  end
 end
+
