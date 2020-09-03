0c32387a887164108336deb27aa2f481e97cc1cd
muuminmuumin <izgomakomatw3623@gmail.com>
2020-09-03 11:09:33 -0700

    GroupEditFunction

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,12 +13,23 @@ class GroupsController < ApplicationController
       render :new
     end
   end
-  
-  private
-  
-  def group_params
-    params.require(:group).permit(:name, user_ids: [])
+
+  def edit
+    @group = Group.find(params[:id])
   end
 
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [] )
+  end
 
 end
