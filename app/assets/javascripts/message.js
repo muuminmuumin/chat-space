67b1302a717b1c4fa8fd769d7caf7fe3bf7a9634
muuminmuumin <izgomakomatw3623@gmail.com>
2020-09-13 14:26:29 -0700

    5AjaxMessagesCreateMove

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,17 @@
 $(function(){
   $('.Form').on('submit', function(e){
     e.preventDefault()
-    console.log("さよなら")
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,  
+      type: 'POST',  
+      data: formData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    
+    
   });
 });
