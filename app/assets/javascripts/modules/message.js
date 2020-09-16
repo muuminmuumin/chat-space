58199a17ca8dd4763826c95ece09ba9f2df401c0
muuminmuumin <izgomakomatw3623@gmail.com>
2020-09-16 09:05:12 +0900

    5 Brushup

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -71,35 +71,36 @@ $(function(){
   });
   
 
-  let reloadMessages = function() {
+  //let reloadMessages = function() {
     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    let last_message_id = $('.MessageFrame:last').data("message-id") || 0;
-    $.ajax({
+    //let last_message_id = $('.MessageFrame:last').data("message-id") || 0;
+    //$.ajax({
       //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-      url: "api/messages",
+      //url: "api/messages",
       //ルーティングで設定した通りhttpメソッドをgetに指定
-      type: 'get',
-      dataType: 'json',
+      //type: 'get',
+      //dataType: 'json',
       //dataオプションでリクエストに値を含める
-      data: {id: last_message_id}
-    })
-    .done(function(messages) {
+      //data: {id: last_message_id}
+    //})
+    //.done(function(messages) {
       // 更新するメッセージがなかった場合は.doneの後の処理が動かないようにする
-      if (messages.length !== 0) {
+      //if (messages.length !== 0) {
         //追加するHTMLの入れ物を作る
-        let insertHTML = '';
+        //let insertHTML = '';
         //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-        $.each(messages, function(i, message) {
-          insertHTML += buildHTML(message)
-        });
+        //$.each(messages, function(i, message) {
+          //insertHTML += buildHTML(message)
+        //});
         //メッセージが入ったHTMLに、入れ物ごと追加
-        $('.MessageList').append(insertHTML);
-      }
-    })
-    .fail(function() {
-      alert('error');
-    });
-  };
+        //$('.MessageList').append(insertHTML);
+        //$('.MessageList').animate({ scrollTop: $('.MessageList')[0].scrollHeight});
+      //}
+    //})
+    //.fail(function() {
+      //alert('error');
+    //});
+  //};
 
-  setInterval(reloadMessages, 7000);
+  //setInterval(reloadMessages, 7000);
 });
